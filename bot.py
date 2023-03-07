5f257391ce55decf8af75c962cc1ef326dcc4f8c
Abhimohannew <90506556+Abhimohannew@users.noreply.github.com>
2023-03-07 14:30:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            name="renamer",
+            name="pyro-rename-bot",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
